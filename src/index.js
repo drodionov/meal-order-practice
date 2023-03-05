d03b6d30acf4a95eb0aae817b42f4f11f76a3951
Dmitrii Rodionov <sf.zeon@gmail.com>
2023-03-05 19:32:00 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {StrictMode} from 'react';
 import App from './App'
+import {createRoot} from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <StrictMode>
       <App/>
-    </React.StrictMode>
+    </StrictMode>
 )
