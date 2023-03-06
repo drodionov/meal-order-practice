7f9d8fad1049c708f2761d00684ef5d277c434e7
Dmitrii Rodionov <sf.zeon@gmail.com>
2023-03-06 22:03:51 +0300

    First version of the Header component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom/client'
 import App from './App'
-import {createRoot} from "react-dom";
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <StrictMode>
       <App/>
